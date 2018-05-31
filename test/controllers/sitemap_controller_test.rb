fa41f8c672e014ea3fbf8c1eb4c6748ceca54c62
Malachai Frazier <malachaifrazier@users.noreply.github.com>
2018-05-30 20:09:15 -0400

    test DIR removed

--- test/controllers/sitemap_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SitemapControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get sitemap_index_url
-    assert_response :success
-  end
-
-end
