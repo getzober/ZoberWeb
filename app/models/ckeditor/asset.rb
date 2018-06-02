169331c4feb389adbd085d87738f2b31d92f4b37
Malachai Frazier <malachaifrazier@users.noreply.github.com>
2018-06-01 20:07:18 -0400

    added initializers/ckeditor.rb & missing Ckeditor::Asset includes

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -1,5 +1,7 @@
 class Ckeditor::Asset < ActiveRecord::Base
+  include ActiveRecord
   include Ckeditor::Orm::ActiveRecord::AssetBase
+  include Ckeditor::Backend::CarrierWave
 
   delegate :url, :current_path, :content_type, to: :data
 
