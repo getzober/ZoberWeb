85456296a39df7212bc887c19aef1d4cb521524f
jaredloucks <jaredbrianming@gmail.com>
2018-08-03 13:25:07 -0600

    changed name of treatment "type" column to "style" to avoid Rails reserved-column-name errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180802172831) do
+ActiveRecord::Schema.define(version: 20180803192349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -245,7 +245,7 @@ ActiveRecord::Schema.define(version: 20180802172831) do
 
   create_table "treatments", force: :cascade do |t|
     t.string "name"
-    t.integer "type"
+    t.integer "style"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
