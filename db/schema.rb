93f3872b1e42dacf4a95ef180cd6a8361bfb1680
jaredloucks <jaredbrianming@gmail.com>
2018-08-07 10:07:12 -0600

    ran db:reset and reinstalled axios

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180803192349) do
-
+ActiveRecord::Schema.define(version: 20180731220558) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -215,13 +214,6 @@ ActiveRecord::Schema.define(version: 20180803192349) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "uuid"
-  end
-
-  create_table "states", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "full_name"
-    t.string "abbreviation"
   end
 
   create_table "topics", force: :cascade do |t|
