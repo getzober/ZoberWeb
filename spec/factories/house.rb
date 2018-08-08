5fa5947062bd80386b2a770cb974ab8435303f4a
jaredloucks <jaredbrianming@gmail.com>
2018-08-08 10:11:12 -0600

    added new house columns to factory_bot

--- spec/factories/house.rb
+++ spec/factories/house.rb
@@ -14,7 +14,7 @@ FactoryBot.define do
     capacity 100
     price 5000.00
     deposit 5000.00
-    property_description "A cool place for cool people."
+    property_description "A cool place for cool people"
     neighborhood "The best one"
     curfew "10 PM"
     overnight_passes "Twice per month"
@@ -22,5 +22,12 @@ FactoryBot.define do
     longitude -122.4553
     street2 "Suite 1"
     phone "1234567890"
+    bio "Founded in 1902, the House of Soberness has stayed true to its mission statement of caring for all those in need ever since."
+    house_type "Detox and residential"
+    number_of_diagnoses 2
+    electronics false
+    smoking "Only in designated outdoor areas"
+    gender "Co-ed"
+    room_type "Private and semi-private"
   end
 end
