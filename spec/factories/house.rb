dd1b0f420ba1e14a2993da1e5cd22107bf0047fe
jaredloucks <jaredbrianming@gmail.com>
2018-08-09 16:27:38 -0600

    passing tests for filters_with_category method

--- spec/factories/house.rb
+++ spec/factories/house.rb
@@ -23,12 +23,5 @@ FactoryBot.define do
     street2 "Suite 1"
     phone "1234567890"
     bio "Founded in 1902, the House of Soberness has stayed true to its mission statement of caring for all those in need ever since."
-    house_type "Detox and residential"
-    number_of_diagnoses 1
-    electronics false
-    smoking "Only in designated outdoor areas"
-    gender "Co-ed"
-    room_type "Private and semi-private"
-    recovery_plan "Traditional | 12 step"
   end
 end
