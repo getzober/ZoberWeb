3381dea5e2e7daf283f12925db47b2d18e4cb5a2
jaredloucks <jaredbrianming@gmail.com>
2018-08-09 16:09:49 -0600

    wrote House method filters_with_category()

--- app/models/house.rb
+++ app/models/house.rb
@@ -12,4 +12,17 @@ class House < ApplicationRecord
   def self.search( search = '' )
     where("city ~* ? OR state ~* ? OR zip_code = ?", search, search, search.to_i ).order("created_at DESC")
   end
+
+  def filters_with_category( category_name )
+    # Returns an array for multiple filters, or a single filter if only one exists.
+    category_id = Category.find_by( category: category_name ).id
+    filters_to_return = self.filters.where( "category_id = ?", category_id)
+    if filters_to_return.empty?
+      false
+    elsif filters_to_return.count > 1
+      filters_to_return.all
+    else
+      filters_to_return.first
+    end
+  end
 end
