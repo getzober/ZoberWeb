66c142aa5707f74eb15f9cf7b483bc1aca11eaba
jaredloucks <jaredbrianming@gmail.com>
2018-08-09 13:51:02 -0600

    rolled back house changes to match updated schema

--- app/models/house.rb
+++ app/models/house.rb
@@ -3,10 +3,6 @@ class House < ApplicationRecord
   has_many :house_filters
   has_many :filters, through: :house_filters
   belongs_to :provider, optional: true
-  has_and_belongs_to_many :amenities
-  has_and_belongs_to_many :accreditations
-  has_and_belongs_to_many :insurance_companies
-  has_and_belongs_to_many :treatments
   geocoded_by :address
   
   def address
@@ -15,29 +11,5 @@ class House < ApplicationRecord
 
   def self.search( search = '' )
     where("city ~* ? OR state ~* ? OR zip_code = ?", search, search, search.to_i ).order("created_at DESC")
-  end
-
-  def diagnosis_description
-    diagnoses_integer = self.number_of_diagnoses
-    if diagnoses_integer == 1
-      plurality = 'Single'
-    elsif diagnoses_integer == 2
-      plurality = 'Double'
-    elsif diagnoses_integer == 3
-      plurality = 'Triple'
-    elsif diagnoses_integer == 4
-      plurality = 'Quadruple'
-    else
-      plurality = 'At least quadruple'
-    end
-    return plurality + ' diagnosis'
-  end
-
-  def smoking_policy
-    if self.smoking
-      'Smoking allowed'
-    else
-      'No smoking allowed'
-    end
   end
 end
--- spec/models/house_spec.rb
+++ spec/models/house_spec.rb
@@ -2,17 +2,5 @@ RSpec.describe House, type: :model do
   let!( :house1 ){ create( :house ) }
   let!( :house2 ){ create( :house ) }
 
-  it "diagnosis description displays properly" do
-    house1.update( number_of_diagnoses: 1)
-    expect( house1.diagnosis_description ).to eq( "Single diagnosis" )
-    house2.update( number_of_diagnoses: 5)
-    expect( house2.diagnosis_description ).to eq( "At least quadruple diagnosis" )
-  end
 
-  it "electronics policy displays properly" do
-    house1.update( smoking: true )
-    expect( house1.smoking_policy ).to eq( "Smoking allowed" )
-    house2.update( smoking: false )
-    expect( house2.smoking_policy ).to eq( "No smoking allowed" )
-  end
 end
